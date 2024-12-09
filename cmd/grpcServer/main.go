08072029501ba10fb672c46227f0a3b95388d9e8
Vitor Carrilho <vitor.carrilho@hotmail.com>
2024-12-09 19:11:16 -0300

    evans install

--- cmd/grpcServer/main.go
+++ cmd/grpcServer/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"log"
 	"net"
 
 	"github.com/carrilh0/grpc/internal/database"
@@ -9,15 +10,15 @@ import (
 	"github.com/carrilh0/grpc/internal/service"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
 	db, err := sql.Open("sqlite3", "./db.sqlite")
-
 	if err != nil {
 		panic(err)
 	}
-
 	defer db.Close()
 
 	categoryDb := database.NewCategory(db)
@@ -28,6 +29,7 @@ func main() {
 	reflection.Register(grpcServer)
 
 	lis, err := net.Listen("tcp", ":50051")
+	log.Println("Starting gRPC server on :50051")
 
 	if err != nil {
 		panic(err)
@@ -36,4 +38,5 @@ func main() {
 	if err := grpcServer.Serve(lis); err != nil {
 		panic(err)
 	}
+
 }
